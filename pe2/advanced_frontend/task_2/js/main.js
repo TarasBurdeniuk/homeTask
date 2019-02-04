999d6f8c0d390218b6e9bfb85a7289e9b5f35dc7
Taras Burdeniuk <bur.taras@gmail.com>
2019-02-04 17:15:39 +0200

    change task_2

--- pe2/advanced_frontend/task_2/js/main.js
+++ pe2/advanced_frontend/task_2/js/main.js
@@ -3,5 +3,5 @@ const navMenu = document.getElementsByClassName('nav-menu');
 navMenu[0].addEventListener('click', function (e) {
     e.preventDefault();
     this.classList.toggle('nav-menu-active');
-    document.getElementsByClassName('menu-nav')[0].classList.toggle('menu-nav_active');
+    document.getElementsByClassName('main-menu')[0].classList.toggle('menu-nav_active');
 });
