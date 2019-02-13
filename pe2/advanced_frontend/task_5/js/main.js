2bf1cad45303662d5e2123f8872f398dbe2d44ed
Taras Burdeniuk <bur.taras@gmail.com>
2019-02-13 13:14:39 +0200

    add task_5

---
+++ pe2/advanced_frontend/task_5/js/main.js
@@ -0,0 +1,32 @@
+const body = document.getElementsByTagName('body')[0];
+
+const script = document.getElementsByTagName('script')[0];
+parentElem = script.parentNode;
+const tableElem = document.createElement('table');
+parentElem.insertBefore(tableElem, script);
+
+const table = document.getElementsByTagName('table')[0];
+
+for (let i = 0; i < 30; i++) {
+    const trElem = document.createElement('tr');
+    table.appendChild(trElem);
+    for (let j = 0; j < 30; j++) {
+        const tdElem = document.createElement('td');
+        const row = document.getElementsByTagName('tr')[i];
+        row.appendChild(tdElem);
+    }
+}
+
+function addBack(event) {
+    console.log(event.target);
+    if (event.target === body) {
+        const table = document.getElementsByTagName('table');
+        table[0].classList.toggle('blackBack');
+    } else {
+        event.target.classList.toggle('blackBack');
+    }
+}
+
+body.addEventListener('click', addBack);
+
+
